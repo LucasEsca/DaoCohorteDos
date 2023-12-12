3f49f00158848e0cf15d38153bdd3ee3b5529271
Fair Lucas <lukitaa.27@gmail.com>
2023-12-12 19:15:00 -0300

    first commid

---
+++ src/main/java/Dao/Main.java
@@ -0,0 +1,39 @@
+package Dao;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    //atributos de conexion
+    private Connection conectar = null;
+    
+    private final String contrasena = "123456789";
+    
+    private final String usuario = "root";
+    
+    private final String db = "cohortejava";
+    
+    private final String ip = "localhost";
+    
+    private final String puerto = "3306";
+    
+    private final String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + db;
+    
+    public Connection establecerConeccion(){
+        try{
+            conectar = DriverManager.getConnection(cadena, usuario, contrasena);
+           JOptionPane.showMessageDialog(null, "Se conecto correctamente");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "No se puedo conectar" + e.toString());
+        }
+        return conectar;
+    }
+    
+    
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.establecerConeccion();
+        
+    }
+}
---
+++ src/main/java/Personas/Empleado.java
@@ -0,0 +1,29 @@
+
+package Personas;
+
+public class Empleado {
+    
+    private int id;
+    
+    private String empleado;
+    
+    public Empleado(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(String empleado) {
+        this.empleado = empleado;
+    }
+    
+    
+}
